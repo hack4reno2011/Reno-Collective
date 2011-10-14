6c03011f0eba9f719245929dc7662e884932e60a
Chris Yoder <yodasan@gmail.com>
2011-10-13 23:39:35 -0700

    Test

--- app.rb
+++ app.rb
@@ -2,10 +2,23 @@ require 'rubygems'
 require 'sinatra'
 require 'tropo-webapi-ruby'
 
+enable :sessions
+
 get '/' do
   "Hello World"
 end
 
 post '/start.json' do
-  p Tropo::Generator.parse request.env['rack.input'].read
+  tropo_session = Tropo::Generator.parse request.env["rack.input"].read
+  session[:callid] = tropo_session[:session][:id]
+  tropo = Tropo::Generator.new do
+    say :value => 'Hello World!'
+    on :event => 'hangup', :next => '/hangup.json'
+  end
+  tropo.response
+end
+
+post '/hangup.json' do
+  tropo_event = Tropo::Generator.parse request.env["rack.input"].read
+  p tropo_event
 end
